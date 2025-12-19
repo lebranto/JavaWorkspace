2d0d864efe7d4ce6ebd2d6ff04af62ab30b80c3d
user1 <user1@DESKTOP-GMG48RP>
2025-12-19 17:25:13 +0900

    반가워 수정

--- 01_MyFirstProject/src/com/kh/first/FirstMyObject.java
+++ 01_MyFirstProject/src/com/kh/first/FirstMyObject.java
@@ -11,6 +11,7 @@ public class FirstMyObject {
 	//자바 실행하기 위해 반드시 main 메서드가 존재해야 한다.
 	public static void main(String[] args) {
 		System.out.println("안녕!");
+		System.out.println("반가워");
 		
 		System.out.println(new Date());
 	}
